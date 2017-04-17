17656d0d963a235c0af4da22fe95dd411e27ee5b
Martin Winter <git@macwinnie.me>
2017-04-17 17:08:08 +0200

    add road sign “construction area” (dynamic SVG generated by PHP-Code)

---
+++ baustelle.php
@@ -0,0 +1,25 @@
+<?php
+header('content-type: image/svg+xml; charset=utf-8');
+mb_internal_encoding("UTF-8");
+
+require_once(__DIR__ . DIRECTORY_SEPARATOR . 'vendor/autoload.php');
+require_once(__DIR__ . DIRECTORY_SEPARATOR . 'function.php');
+
+echo '<?xml version="1.0" encoding="utf-8"?>';
+?>
+<svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="-292 385.1 26 22.9" enable-background="new -292 385.1 26 22.9" xml:space="preserve">
+	<g id="svg2" inkscape:version="0.48.2 r9819" sodipodi:docname="AJAX1.svg" xmlns:cc="http://creativecommons.org/ns#" xmlns:sodipodi="http://sodipodi.sourceforge.net/DTD/sodipodi-0.dtd" xmlns:rdf="http://www.w3.org/1999/02/22-rdf-syntax-ns#" xmlns:ns="http://ns.adobe.com/SaveForWeb/1.0/" xmlns:inkscape="http://www.inkscape.org/namespaces/inkscape" xmlns:svg="http://www.w3.org/2000/svg" xmlns:dc="http://purl.org/dc/elements/1.1/">
+		<sodipodi:namedview  inkscape:current-layer="svg2" inkscape:window-width="1280" fit-margin-right="0" inkscape:window-height="1004" inkscape:pageshadow="2" inkscape:window-y="-8" inkscape:window-x="-8" inkscape:window-maximized="1" inkscape:pageopacity="0" fit-margin-bottom="0" id="namedview9" inkscape:cy="233.35287" objecttolerance="10" inkscape:zoom="1.011377" showgrid="false" fit-margin-left="0" inkscape:cx="390.3144" fit-margin-top="0" gridtolerance="10" pagecolor="<?= env('COLOR', '#ffffff') ?>" bordercolor="<?= env('COLOR', '#ffffff') ?>" borderopacity="1" guidetolerance="10"></sodipodi:namedview>
+		<path fill="<?= env('COLOR', '#ffffff') ?>" d="M-279.6,401.3c0,0,0-0.1,0-0.1l-0.9-1.4l-1.4-0.7l-2.1,4c-0.1,0.1-0.1,0.3-0.1,0.4c0,0.3,0.2,0.6,0.5,0.8 l2.2-4.2l0.9,1.4l0.1,1.9c0,0.5,0.4,0.9,0.9,0.9c0,0,0,0,0,0L-279.6,401.3L-279.6,401.3z"/>
+		<circle fill="<?= env('COLOR', '#ffffff') ?>" cx="-278" cy="395.3" r="0.8"/>
+		<path fill="<?= env('COLOR', '#ffffff') ?>" d="M-276.8,401.8l-2.1-1.7l0.2-3.3c0,0,0,0,0-0.1c0-0.5-0.4-0.9-0.9-0.9h-2c-0.2,0-0.4,0.1-0.5,0.3l-0.6,1.2 c-0.1,0.1-0.1,0.2-0.1,0.4c0,0.3,0.2,0.6,0.4,0.7l0.2-0.4l0.5,0.4l0.2-0.3l0,0l-0.5-0.4l0.5-1h0.9l-1.2,2.3l1.4,0.7l0.8-1.6 l-0.1,1.4l-0.4-0.3l-0.2,0.3l0.5,0.4l0,0.2c0,0,0,0,0,0.1c0,0.4,0.3,0.8,0.7,0.8l0-0.4l1.8,1.5C-277,402-276.9,401.9-276.8,401.8z"/>
+		<path fill="<?= env('COLOR', '#ffffff') ?>" d="M-274.1,400.9c-0.2-0.3-0.5-0.4-0.9-0.4c-0.3,0-0.6,0.1-0.9,0.4l-0.9,1c-0.1,0.1-0.2,0.2-0.4,0.3l0,0
+		l-0.2,0.1c-0.1,0-0.1,0.1-0.2,0.1l-1.3,1.5c0,0-0.1,0.1-0.1,0.1c0,0.1,0.1,0.2,0.2,0.2h6.7c0.1,0,0.2-0.1,0.2-0.2c0,0,0-0.1,0-0.1 L-274.1,400.9z"/>
+	</g>
+	<g id="Layer_2">
+		<path fill="<?= env('COLOR', '#ffffff') ?>" d="M-267.8,407.4h-22.3c-0.4,0-0.8-0.2-1-0.6s-0.2-0.8,0-1.2l11.2-19.2c0.2-0.4,0.6-0.6,1-0.6s0.8,0.2,1,0.6 l11.2,19.2c0.2,0.4,0.2,0.8,0,1.2C-267,407.1-267.4,407.4-267.8,407.4z M-288,405h18.1l-9.1-15.6L-288,405z"/>
+	</g>
+	<g>
+		<path fill="<?= env('COLOR', '#ffffff') ?>" d="M-279,385.5c0.5,0,1,0.3,1.3,0.7l11.2,19.2c0.3,0.5,0.3,1,0,1.5c-0.3,0.5-0.8,0.8-1.3,0.8h-22.3 c-0.5,0-1-0.3-1.3-0.8c-0.3-0.5-0.3-1,0-1.5l11.2-19.2C-280,385.7-279.5,385.5-279,385.5 M-279,385.2c-0.6,0-1.2,0.3-1.5,0.9 l-11.2,19.2c-0.3,0.6-0.3,1.2,0,1.8c0.3,0.6,0.9,0.9,1.6,0.9h22.3c0.6,0,1.2-0.3,1.6-0.9c0.3-0.6,0.3-1.2,0-1.8l-11.2-19.2 C-277.7,385.5-278.3,385.2-279,385.2L-279,385.2z"/>
+	</g>
+</svg>
