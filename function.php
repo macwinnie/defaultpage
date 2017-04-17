eece644cddcc0e4ad87713f9ac658e62b17cc36a
Martin Winter <git@macwinnie.me>
2017-04-17 23:57:43 +0200

    bug fixing major functionalities

--- function.php
+++ function.php
@@ -10,6 +10,24 @@ function relativeURL () {
     return substr($dir, strlen($_SERVER['DOCUMENT_ROOT']));
 }
 
+function startsWith($haystack, $needles) {
+    foreach ((array) $needles as $needle) {
+        if ($needle != '' && substr($haystack, 0, strlen($needle)) === (string) $needle) {
+            return true;
+        }
+    }
+    return false;
+}
+
+function endsWith($haystack, $needles) {
+    foreach ((array) $needles as $needle) {
+        if (substr($haystack, -strlen($needle)) === (string) $needle) {
+            return true;
+        }
+    }
+    return false;
+}
+
 if (! function_exists('env')) {
     function env($key, $default = null) {
         $value = getenv($key);
